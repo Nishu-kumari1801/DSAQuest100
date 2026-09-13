6a6e1c9567b4e66b02056aae9ad672ec7fa1ae0c
Nishu Kumari <nishujha.nj01@gmail.com>
2026-09-13 11:21:56 +0530

    Time: 3 ms (11.29%), Space: 8.9 MB (7.27%) - LeetHub

---
+++ 0771-jewels-and-stones/0771-jewels-and-stones.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int numJewelsInStones(string jewels, string stones) {
+        unordered_set<int>st;
+        for(char c:jewels) st.insert(c);
+        int cnt =0;
+        for(char c:stones){
+            if(st.find(c) != st.end()) cnt++;
+        }
+        return cnt;
+    }
+};
